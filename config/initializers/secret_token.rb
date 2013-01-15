cfbdfa922344fc6a23a70e0ee49607a704a4b7e0
Prasad Madala <bhanuprasad143@gmail.com>
2013-01-15 10:07:50 +0700

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dimarsec::Application.config.secret_token = '13e805f728b0b182dcda9e50e6023cadf3578973f312a8f13a2dbc3d8e7205ba1e67e653ab8b27741c5612a35a21496e8359f9431bbe2062f0601ed6ada59abf'
